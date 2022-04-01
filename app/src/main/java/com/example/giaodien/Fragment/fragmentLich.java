8f791d35424f6a9141ca967120c27c9dae331446
quangbui478@gmail.com <quangbui478@gmail.com>
2022-04-02 00:48:16 +0700

    cập nhật Fragment 2

--- app/src/main/java/com/example/giaodien/Fragment/fragmentLich.java
+++ app/src/main/java/com/example/giaodien/Fragment/fragmentLich.java
@@ -8,10 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.giaodien.R;
+
 public class fragmentLich extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_lich,container,false);
     }
 }
--- app/src/main/java/com/example/giaodien/MainActivity.java
+++ app/src/main/java/com/example/giaodien/MainActivity.java
@@ -1,26 +1,74 @@
 package com.example.giaodien;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.giaodien.Fragment.fragmentAdd;
+import com.example.giaodien.Fragment.fragmentCongViec;
+import com.example.giaodien.Fragment.fragmentLich;
+import com.example.giaodien.Fragment.fragmentThongKe;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLich;
+    private Button btnThongKe;
+    private Button btnNote;
+    private Button btnCongViec;
+    private Button btnOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        innerFragment();
     }
 
-    public void PersonClick(View view) {
-        Intent intent = new Intent(MainActivity.this, InfoActivity.class);
-        startActivity(intent);
+    public void innerFragment()
+    {
+        fragmentLich firstFragment =  new fragmentLich();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.Frame_Layout,firstFragment);
+        ft.commit();
+    }
+
+    public void addFragment(Fragment fragment)
+    {
+        FragmentManager fgt = getSupportFragmentManager();
+        FragmentTransaction ft = fgt.beginTransaction();
+        ft.add(R.id.Frame_Layout, fragment);
+        ft.addToBackStack(fragment.getClass().getSimpleName());
+        ft.commit();
+
+    }
+    public void oneClick(View view) {
+
+        switch (view.getId())
+        {
+            case R.id.btn_Lich:
+               addFragment( new fragmentLich());
+                break;
+            case R.id.btn_note:
+                addFragment(new fragmentCongViec());
+                break;
+            case R.id.btn_ThongKe:
+                addFragment(new fragmentThongKe());
+                break;
+            case R.id.btn_person:
+                Intent intent =new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+                break;
+
+        }
     }
 
 
-    public void CongViecClick(View view) {
-        Intent intent = new Intent(MainActivity.this, Add_Job.class);
-        startActivity(intent);
-    }
 }
